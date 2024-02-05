90d3ed6bb20f36e66adc5f3dbb81165a538afec3
notjustdevops <Not.Just.DevOps@gmail.com>
2024-02-05 16:36:49 +0200

    day 14 start

---
+++ day_14_higher_lower_game/main.py
@@ -0,0 +1,35 @@
+#Import Random
+from random import randint
+
+#Create library - ready.
+
+
+# Generate random number to pick from the library
+# Random Pick firts key: value
+
+# Generate random number to pick from the library
+# Random Pick a second number
+
+# Print First "Key" has "Value" average monthly searches
+# vs 
+# Print Second "Key" has "Value" average monthly searches
+
+# Ask if the second key:value pair has a more monthly searches?
+# input: 'Higher' enter h, for lower enter l lower()
+
+# Compare: Checjk if the second value highrt or lower.
+# If second value Higher or Lower is True   - second key:value becomes first
+# Continue to play
+# Pick new pair from game_data - it becoms second
+
+# If answer for  HIgher or Lower is False
+# End the Game
+# Print Score
+# Ask if player wants to play a new game  
+
+# Count the score
+# Print the current score
+
+# Collect the score of each game 
+# Print the Highest Score of the previous games
+
