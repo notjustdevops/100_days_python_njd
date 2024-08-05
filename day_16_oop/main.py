271e020a64dea1c7877e324bf95b91efa90cb8b6
notjustdevops <Not.Just.DevOps@gmail.com>
2024-08-05 11:37:57 +0300

    day_16

---
+++ day_16_oop/main.py
@@ -0,0 +1,26 @@
+#import another_module
+#print(another_module.another_variable)
+
+
+#from turtle import Turtle, Screen
+#timmy = Turtle()
+#print(timmy)
+#timmy.shape("turtle")
+#timmy.color("green")
+#for move in range (10):
+#    timmy.forward(100)
+#    timmy.left(50)
+#    timmy.forward(100)
+#    timmy.home()
+#
+#
+#my_screen = Screen()
+#print(my_screen.canvheight)
+#my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.field_
+
+
+print(table)
