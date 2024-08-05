02f942afc73c041f50f0c17f820f9fdb7f4f6c19
notjustdevops <Not.Just.DevOps@gmail.com>
2024-08-05 06:54:40 +0300

    residual commit from linux comp

--- day_14_higher_lower_game/main.py
+++ day_14_higher_lower_game/main.py
@@ -1,24 +1,36 @@
 #Import Random
-from random import randint
-
+import random
+from art import logo
 #Create library - ready.
-
-
-# Generate random number to pick from the library
-# Random Pick firts key: value
+from game_data import data
 
 # Generate random number to pick from the library
-# Random Pick a second number
+def random_data():
+    dict_item = random.choice(data)
+    print(f"\n   '{dict_item['name']}'\n    has\n    {dict_item['follower_count']}\n    average monthly searches\n")
 
 # Print First "Key" has "Value" average monthly searches
 # vs 
 # Print Second "Key" has "Value" average monthly searches
+    
+# Random Pick a second number
+# Random Pick firts key: value
+    
+def game():
+    random_data_first = random_data()
+    print("vs")
+    random_data_second = random_data()
 
+game()
+
+# Generate random number to pick from the library
 # Ask if the second key:value pair has a more monthly searches?
+
 # input: 'Higher' enter h, for lower enter l lower()
 
 # Compare: Checjk if the second value highrt or lower.
 # If second value Higher or Lower is True   - second key:value becomes first
+
 # Continue to play
 # Pick new pair from game_data - it becoms second
 
@@ -32,4 +44,3 @@ from random import randint
 
 # Collect the score of each game 
 # Print the Highest Score of the previous games
-
